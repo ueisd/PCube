97fe25a23673332c5a99378ad8ae7d798085a774
btaousoussema <btaousoussema@hotmail.com>
2020-06-19 11:24:42 -0400

    Ajout des vérificateurs du formulaire pour la création d'users. Ajout de classes pour le formulaire de User.

--- PCube/src/app/components/pages/add-user/add-user.component.ts
+++ PCube/src/app/components/pages/add-user/add-user.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service'
-import { User } from 'src/app/Model/user';
-
+import { UserForm } from 'src/app/Model/user-form';
+import { RoleService } from 'src/app/services/role.service'
+import { Role } from 'src/app/Model/role';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-user',
@@ -13,30 +15,70 @@ import { User } from 'src/app/Model/user';
 export class AddUserComponent implements OnInit {
   authenticated: boolean;
   accessLevel: number;
+  roles: Role[];
+  userForm : FormGroup;
+  @Input() isChecked = false;
 
   @Output() createUser : EventEmitter<any> = new EventEmitter;
-  userForm = new FormGroup({
-    name : new FormControl('', Validators.required),
-    lname : new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required)
-  });
 
-  constructor(private router: Router, private userservice:UserService){ 
+
+  constructor(private router: Router, private userService:UserService, 
+    private roleService: RoleService, private dialog: MatDialog, private fb: FormBuilder){ 
   }
 
   onSubmit(){
     const name = this.userForm.get("name").value;
     const lname = this.userForm.get("lname").value;
     const email = this.userForm.get("email").value;
-    const user = {name, lname, email};
-    console.log(name, lname);
-    this.userservice.createUser(user);
-    //this.createUser.emit(user);
-    this.router.navigate(['/addUser']);
+    const password = this.userForm.get("password").value
+    const roleId = this.userForm.get("roles").value;
+    const creerUser = this.isChecked;
+    const passwordConfirmation = this.userForm.get("passwordConfirmation").value;
+    if (this.passwordsMatch(password, passwordConfirmation)){
+      let user = new UserForm(name, lname, email, password, passwordConfirmation, roleId, creerUser);
+      this.userService.createUser(user);
+      this.router.navigate(['/'])
+    }else{
+      console.log(passwordConfirmation, password, password === passwordConfirmation)
+    }
+    
   }
   
-  ngOnInit(): void {
-
+  ngOnInit(): void { 
+    this.roleService.getRoless().subscribe(res=>{
+      this.roles= res;
+    });
+    this.userForm = this.fb.group({
+      name : ['', Validators.required],
+      lname : ['', Validators.required],
+      email : ['', Validators.required], 
+      password: ['', Validators.required],
+      passwordConfirmation: ['', Validators.required],
+      roles: ['', Validators.required]
+    },{validators: this.passwordMatchValidator});
   }
 
+  clicked = (any) => {  
+    const email = this.userForm.get('email');
+    const password = this.userForm.get('password');
+    const passwordConfirmation = this.userForm.get('passwordConfirmation');
+    email.disabled ? email.enable() : email.disable();
+    password.disabled ? password.enable() : password.disable();
+    passwordConfirmation.disabled ? passwordConfirmation.enable() : passwordConfirmation.disable();
+    this.isChecked = !this.isChecked;
+    }
+
+    passwordsMatch(password:string, confirmation:string){
+     return password === confirmation;
+    }  
+
+    passwordMatchValidator(control : AbstractControl ){
+      let password = control.get("password").value;
+      let confirmPassword = control.get("passwordConfirmation").value;
+      if (password === confirmPassword){
+        return null;
+      }else{
+        control.get('passwordConfirmation').setErrors({ notMatching : true });
+      }
+    }
 }
--- PCube/src/app/services/user.service.ts
+++ PCube/src/app/services/user.service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { User } from 'src/app/Model/user';
+import { UserForm } from 'src/app/Model/user-form';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   POST_USER_API = '/api/auth/user';
-  private jwt: JwtHelperService = new JwtHelperService();
-
  
   constructor(private http : HttpClient) {
    }
 
-  createUser(user:User) {
-    console.log("ON est dans le create!!");
-    console.log(this.POST_USER_API);
-    this.http.post<User>(this.POST_USER_API, {User}).subscribe({
+  createUser(user:UserForm) {
+    this.http.post<UserForm>(this.POST_USER_API, user).subscribe({
       error: error => console.error("There is an error!", error)
     })
   }
