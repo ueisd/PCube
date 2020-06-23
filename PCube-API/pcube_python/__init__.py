f0b8ab7d7f97eb253aea88dcded73b52ea4568a3
ARCOLE Alexandre-Thibault <arcole.alexandre-thibault@courrier.uqam.ca>
2020-06-22 21:12:45 -0400

    ajout des fonction (flask et sql) pour recuperer la liste de tout les utilisateurs

--- PCube-API/pcube_python/__init__.py
+++ PCube-API/pcube_python/__init__.py
@@ -4,7 +4,9 @@ from .main import main
 from .autre_routes import test
 from .auth_routes import auth
 from .project_routes import project
+from .user_routes import user
 from flask_jwt_extended import JWTManager
+
 
 def create_app():
 
@@ -16,6 +18,7 @@ def create_app():
     app.register_blueprint(test)
     app.register_blueprint(auth, url_prefix='/api/auth')
     app.register_blueprint(project, url_prefix='/api/project')
+    app.register_blueprint(user, url_prefix='/api/user')
 
     JWTManager(app)
 
