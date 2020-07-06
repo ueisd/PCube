4d4768ec70c71b1cb75cb8554d5ccbf526f224ad
Pierre-Luc Maître <pierrelucueisd@gmail.com>
2020-07-06 11:49:37 -0400

    ajout d'activité

--- PCube/src/app/components/domain/activity/activity-list/activity-list.component.ts
+++ PCube/src/app/components/domain/activity/activity-list/activity-list.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivityItem } from 'src/app/models/activity';
+import { MatDialog } from "@angular/material/dialog";
 import { ActivityService } from 'src/app/services/activity/activity.service';
 import * as $ from 'jquery/dist/jquery.min.js';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl } from '@angular/forms';
 import { Observable, } from 'rxjs';
+import { DeleteActivityComponent } from '../delete-activity/delete-activity.component';
 
 const HIDDEN_CLASS = 'hidden';
 
@@ -17,7 +20,9 @@ export class ActivityListComponent implements OnInit {
 
   nameFilter = new FormControl('');
 
-  constructor(private activityService: ActivityService ) { }
+  constructor(private activityService: ActivityService, 
+    private dialog: MatDialog, 
+    private snackBar : MatSnackBar ) { }
 
   displayedColumns : string[];
   dataSource: ActivityItem[];
@@ -71,6 +76,36 @@ export class ActivityListComponent implements OnInit {
     }.bind(this));
   }
 
+  openDeleteDialog(activity) {
+    const dialogRef = this.dialog.open(DeleteActivityComponent, {
+      data: {
+        id: activity.id,
+        name: activity.name,
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result !== undefined) {
+        this.activityService.deleteActivity(result.id).subscribe((data) => {
+          this.openSnackBar('L\'activité a été supprimé!', 'notif-success');
+          this.refreshList();
+        },
+        (error) => {
+          this.openSnackBar('Une erreur s\'est produit. Veuillez réessayer', 'notif-error');
+        });
+      }
+    });
+  }
+
+  openSnackBar(message, panelClass) {
+    this.snackBar.open(message, 'Fermer', {
+      duration: 2000,
+      horizontalPosition: "right",
+      verticalPosition: "bottom",
+      panelClass: [panelClass]
+    });
+  }
+
   refreshList(){
     let activity = new ActivityItem();
     activity.name = this.nameFilter.value.trim();
@@ -79,7 +114,6 @@ export class ActivityListComponent implements OnInit {
       this.dataSource = activities;
       this.bindKeypress();
     });
-    
   }
 
   editAction(activity){
