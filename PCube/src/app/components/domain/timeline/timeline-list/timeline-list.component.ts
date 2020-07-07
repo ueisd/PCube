6433a72b0606a904b001fde0e64eb4e2109bcad5
Stebenne <sebastien706@hotmail.com>
2020-07-07 14:42:31 -0400

    correction d'un bug pour l'affichage de l'information dans le popup

--- PCube/src/app/components/domain/timeline/timeline-list/timeline-list.component.ts
+++ PCube/src/app/components/domain/timeline/timeline-list/timeline-list.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DeleteTimelineComponent } from 'src/app/components/domain/timeline/delete-timeline/delete-timeline.component';
 
+
 @Component({
   selector: 'app-timeline-list',
   templateUrl: './timeline-list.component.html',
@@ -37,12 +38,16 @@ export class TimelineListComponent implements OnInit {
   }
 
   onDeleteTimeline(timeline:any){
-    
     const dialogRef = this.dialog.open(DeleteTimelineComponent, {
       data: { 
-        id: timeline.id, 
-        firstName: timeline.first_name, 
-        lastName: timeline.last_name, 
+        activity_name: timeline.activity_name,
+        day_of_week: timeline.day_of_week,
+        expense_name: timeline.expense_name, 
+        first_name: timeline.first_name,
+        last_name: timeline.last_name,
+        project_name: timeline.project_name,
+        punch_in: timeline.punch_in,
+        punch_out: timeline.punch_out,
       },
       panelClass: 'warning-dialog'
     });
