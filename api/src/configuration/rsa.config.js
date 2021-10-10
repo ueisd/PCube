3beeb657698617a3a23bb0d001b31d50428621be
pierrelucueisd <pierrelucueisd@gmail.com>
2021-10-10 18:25:07 -0400

    maj lecture fichiers

--- api/src/configuration/rsa.config.js
+++ api/src/configuration/rsa.config.js
@@ -2,19 +2,39 @@ const fs = require('fs');
 var nconf = require('nconf');
 
 exports.configRsa = () => {
-    let nconfKeyPrivate = nconf.get('rsa_key_private');
+    let nconfKeyPrivate;
+    try {
+        nconfKeyPrivate = fs.readFileSync('./src/rsa/key', 'utf8')
+    }catch (err) {
+        if(err.code === 'ENOENT') {
+            nconfKeyPrivate = nconf.get('rsa_key_private');
+        }
+    }
+    console.log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+    console.log(nconfKeyPrivate);
+    nconf.set('rsaKeyPrivate', nconfKeyPrivate);
+    /*let nconfKeyPrivate = nconf.get('rsa_key_private');
     if(!nconfKeyPrivate) {
         nconfKeyPrivate = fs.readFileSync('./src/rsa/key', 'utf8');
         nconf.set('rsaKeyPrivate', nconfKeyPrivate);
     } else {
         nconf.set('rsaKeyPrivate', nconfKeyPrivate);
-    }
+    }*/
 
-    let rsaPublicKey = nconf.get('rsa_public_key');
+    let rsaPublicKey;
+    try {
+        rsaPublicKey = fs.readFileSync('./src/rsa/key.pub', 'utf8');
+    } catch (err) {
+        if(err.code === 'ENOENT') {
+            rsaPublicKey = nconf.get('rsa_public_key');
+        }
+    }
+    nconf.set('rsaKeyPublic', rsaPublicKey);
+    /*let rsaPublicKey = nconf.get('rsa_public_key');
     if(!rsaPublicKey) {
         rsaPublicKey = fs.readFileSync('./src/rsa/key.pub', 'utf8');
         nconf.set('rsaKeyPublic', rsaPublicKey);
     } else {
         nconf.set('rsaKeyPublic', rsaPublicKey);
-    }
+    }*/
 }
