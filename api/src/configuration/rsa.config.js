b04d0901e72d18e212a57cd468dff4c4b273ed9a
pierrelucueisd <pierrelucueisd@gmail.com>
2021-10-10 18:29:55 -0400

    maj test

--- api/src/configuration/rsa.config.js
+++ api/src/configuration/rsa.config.js
@@ -3,16 +3,24 @@ var nconf = require('nconf');
 
 exports.configRsa = () => {
     let nconfKeyPrivate;
-    try {
+    fs.stat('./src/rsa/key', function(err, stat) {
+        if(err == null) {
+            console.log('File exists');
+        } else if(err.code === 'ENOENT') {
+            // file does not exist
+            console.log("./src/rsa/key not exist")
+        } else {
+            console.log('Some other error: ', err.code);
+        }
+    });
+    /*try {
         nconfKeyPrivate = fs.readFileSync('./src/rsa/key', 'utf8')
     }catch (err) {
-        if(err.code === 'ENOENT') {
-            nconfKeyPrivate = nconf.get('rsa_key_private');
-        }
-    }
+        nconfKeyPrivate = nconf.get('rsa_key_private'); 
+    }*/
+    nconf.set('rsaKeyPrivate', nconfKeyPrivate);
     console.log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
     console.log(nconfKeyPrivate);
-    nconf.set('rsaKeyPrivate', nconfKeyPrivate);
     /*let nconfKeyPrivate = nconf.get('rsa_key_private');
     if(!nconfKeyPrivate) {
         nconfKeyPrivate = fs.readFileSync('./src/rsa/key', 'utf8');
@@ -21,15 +29,25 @@ exports.configRsa = () => {
         nconf.set('rsaKeyPrivate', nconfKeyPrivate);
     }*/
 
+    fs.stat('./src/rsa/key.pub', function(err, stat) {
+        if(err == null) {
+            console.log('File exists');
+        } else if(err.code === 'ENOENT') {
+            // file does not exist
+            console.log("./src/rsa/key.pub not exist");
+        } else {
+            console.log('Some other error: ', err.code);
+        }
+    });
     let rsaPublicKey;
-    try {
+    /*try {
         rsaPublicKey = fs.readFileSync('./src/rsa/key.pub', 'utf8');
     } catch (err) {
-        if(err.code === 'ENOENT') {
-            rsaPublicKey = nconf.get('rsa_public_key');
-        }
-    }
+        rsaPublicKey = nconf.get('rsa_public_key');
+    }*/
     nconf.set('rsaKeyPublic', rsaPublicKey);
+    console.log("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+    console.log(rsaPublicKey);
     /*let rsaPublicKey = nconf.get('rsa_public_key');
     if(!rsaPublicKey) {
         rsaPublicKey = fs.readFileSync('./src/rsa/key.pub', 'utf8');
