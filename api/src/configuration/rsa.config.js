20444869ad985e2fdd4739ff2a33c63c1a45e503
pierrelucueisd <pierrelucueisd@gmail.com>
2021-10-10 18:32:35 -0400

    test heroku2

--- api/src/configuration/rsa.config.js
+++ api/src/configuration/rsa.config.js
@@ -3,21 +3,11 @@ var nconf = require('nconf');
 
 exports.configRsa = () => {
     let nconfKeyPrivate;
-    fs.stat('./src/rsa/key', function(err, stat) {
-        if(err == null) {
-            console.log('File exists');
-        } else if(err.code === 'ENOENT') {
-            // file does not exist
-            console.log("./src/rsa/key not exist")
-        } else {
-            console.log('Some other error: ', err.code);
-        }
-    });
-    /*try {
+    try {
         nconfKeyPrivate = fs.readFileSync('./src/rsa/key', 'utf8')
     }catch (err) {
         nconfKeyPrivate = nconf.get('rsa_key_private'); 
-    }*/
+    }
     nconf.set('rsaKeyPrivate', nconfKeyPrivate);
     console.log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
     console.log(nconfKeyPrivate);
@@ -29,25 +19,15 @@ exports.configRsa = () => {
         nconf.set('rsaKeyPrivate', nconfKeyPrivate);
     }*/
 
-    fs.stat('./src/rsa/key.pub', function(err, stat) {
-        if(err == null) {
-            console.log('File exists');
-        } else if(err.code === 'ENOENT') {
-            // file does not exist
-            console.log("./src/rsa/key.pub not exist");
-        } else {
-            console.log('Some other error: ', err.code);
-        }
-    });
     let rsaPublicKey;
-    /*try {
+    try {
         rsaPublicKey = fs.readFileSync('./src/rsa/key.pub', 'utf8');
     } catch (err) {
         rsaPublicKey = nconf.get('rsa_public_key');
-    }*/
+    }
     nconf.set('rsaKeyPublic', rsaPublicKey);
-    console.log("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-    console.log(rsaPublicKey);
+    console.log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+    console.log(nconfKeyPrivate);
     /*let rsaPublicKey = nconf.get('rsa_public_key');
     if(!rsaPublicKey) {
         rsaPublicKey = fs.readFileSync('./src/rsa/key.pub', 'utf8');
