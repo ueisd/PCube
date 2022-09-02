1ee1953a620fafb7fa4f965c47c5f6b87c3929ba
pierrelucueisd <pierrelucueisd@hotmail.com>
2022-09-02 06:56:51 -0400

    Prettier code user service

--- client/src/app/services/user/user.service.ts
+++ client/src/app/services/user/user.service.ts
@@ -5,41 +5,30 @@ import { User } from 'src/app/models/user';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-const API_USER = environment.api_url + "/api/user";
-const API_IS_UNIQUE = API_USER + "/emailUnique";
+const API_USER = environment.api_url + '/api/user';
+const API_IS_UNIQUE = API_USER + '/emailUnique';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getAllUser():Observable<User[]> {
-    return this.http.get<User[]>(API_USER).pipe(
-      map(users => users.map(response => 
-        new User(response)
-      ))
-    );
+  getAllUser(): Observable<User[]> {
+    return this.http.get<User[]>(API_USER).pipe(map((users) => users.map((response) => new User(response))));
   }
 
   deleteUser(id): Observable<{}> {
-    return this.http.delete(API_USER + "/" + id);
+    return this.http.delete(API_USER + '/' + id);
   }
 
   isEmailUnique(email): Observable<boolean> {
-    var url = API_IS_UNIQUE + "/" + email
+    const url = API_IS_UNIQUE + '/' + email;
     return this.http.get<boolean>(url);
   }
 
-  modifyUser(id, email, firstName, lastName, roleId : number) {
-    return this.http.put<User>(API_USER, {
-      id: id,
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      RoleId: roleId
-    });
+  modifyUser(id, email, firstName, lastName, roleId: number) {
+    return this.http.put<User>(API_USER, { id, email, firstName, lastName, RoleId: roleId });
   }
 
   createUser(user: User, pwd): Observable<User> {
@@ -49,7 +38,7 @@ export class UserService {
       lastName: user.lastName,
       RoleId: user.role.id,
       roleName: user.role.name,
-      password: pwd
+      password: pwd,
     });
   }
 }
