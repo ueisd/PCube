49aa6caccb9519d8f41b1999c3abcf5307c88fd3
pierrelucueisd <pierrelucueisd@hotmail.com>
2022-09-02 06:52:29 -0400

    Prettier code for client user list

--- client/src/app/components/domain/user/user-list/user-list.component.ts
+++ client/src/app/components/domain/user/user-list/user-list.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from 'src/app/services/user/user.service';
 import { User } from 'src/app/models/user';
-import { MatDialog } from "@angular/material/dialog";
+import { MatDialog } from '@angular/material/dialog';
 import { DeleteUserComponent } from 'src/app/components/domain/user/delete-user/delete-user.component';
 import { ModifyUserComponent } from 'src/app/components/domain/user/modify-user/modify-user.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl } from '@angular/forms';
 import { CustomSnackBar } from 'src/app/utils/custom-snackbar';
 import { MatPaginator } from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
-
-  displayedColumns : string[];
+  displayedColumns: string[];
   dataSource = new MatTableDataSource<User>();
   userList: User[];
 
@@ -26,51 +25,51 @@ export class UserListComponent implements OnInit {
   emailFilter = new FormControl('');
   roleFilter = new FormControl('');
 
-  customSnackBar:CustomSnackBar = new CustomSnackBar(this.snackBar)
+  customSnackBar: CustomSnackBar = new CustomSnackBar(this.snackBar);
 
-  constructor(private userService: UserService,
-    private dialog: MatDialog,
-    private snackBar : MatSnackBar) { }
+  constructor(private userService: UserService, private dialog: MatDialog, private snackBar: MatSnackBar) {}
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
   ngOnInit(): void {
     this.dataSource.paginator = this.paginator;
-    this.userService.getAllUser().toPromise().then(users => {
-      this.userList = users;
-      this.displayedColumns = ['firstName', 'lastName', 'email', 'role', 'operations'];
-      this.dataSource = new MatTableDataSource<User>(users);
-      this.dataSource.paginator = this.paginator;
-    }).catch(err => {
-      // gérer l'ereure
-    });
+    this.userService
+      .getAllUser()
+      .toPromise()
+      .then((users) => {
+        this.userList = users;
+        this.displayedColumns = ['firstName', 'lastName', 'email', 'role', 'operations'];
+        this.dataSource = new MatTableDataSource<User>(users);
+        this.dataSource.paginator = this.paginator;
+      })
+      .catch((err) => {
+        // TODO Gérer l'erreur (PLM)
+        console.log(err);
+      });
   }
 
-  filterListFromUserFilter(user:User, userList): User[] {
-    let regexs = {
-      firstName: new RegExp("^" + user.firstName  + "", "i"),
-      lastName:  new RegExp("^" + user.lastName   + "", "i"),
-      email:      new RegExp("^" + user.email       + "", "i"),
-      role_name:  new RegExp("^" + user.role.name   + "", "i")
-    }
+  filterListFromUserFilter(user: User, userList): User[] {
+    const regex = {
+      firstName: new RegExp('^' + user.firstName + '', 'i'),
+      lastName: new RegExp('^' + user.lastName + '', 'i'),
+      email: new RegExp('^' + user.email + '', 'i'),
+      role_name: new RegExp('^' + user.role.name + '', 'i'),
+    };
 
-    return userList.filter(u => {
-      return regexs.firstName.test(u.firstName) 
-        && regexs.lastName.test(u.lastName)
-        && regexs.email.test(u.email)
-        && regexs.role_name.test(u.role.name);
+    return userList.filter((u) => {
+      return regex.firstName.test(u.firstName) && regex.lastName.test(u.lastName) && regex.email.test(u.email) && regex.role_name.test(u.role.name);
     });
   }
 
   resetFilters() {
-    this.nameFilter.setValue("");
-    this.lastNameFilter.setValue("");
-    this.emailFilter.setValue("");
-    this.roleFilter.setValue("");
+    this.nameFilter.setValue('');
+    this.lastNameFilter.setValue('');
+    this.emailFilter.setValue('');
+    this.roleFilter.setValue('');
   }
 
   getUserFilterFromForm(): User {
-    let user = new User();
+    const user = new User();
     user.firstName = this.nameFilter.value.trim();
     user.lastName = this.lastNameFilter.value.trim();
     user.email = this.emailFilter.value.trim();
@@ -78,9 +77,9 @@ export class UserListComponent implements OnInit {
     return user;
   }
 
-  onFilterChanged(){
-    let userFilter = this.getUserFilterFromForm();
-    let filteredUsers = this.filterListFromUserFilter(userFilter, this.userList);
+  onFilterChanged() {
+    const userFilter = this.getUserFilterFromForm();
+    const filteredUsers = this.filterListFromUserFilter(userFilter, this.userList);
 
     this.dataSource = new MatTableDataSource<User>(filteredUsers);
     this.dataSource.paginator = this.paginator;
@@ -88,48 +87,48 @@ export class UserListComponent implements OnInit {
 
   async openEditDialog(user: User) {
     const dialogRef = this.dialog.open(ModifyUserComponent, {
-      data: { 
-        id: user.id, 
-        firstName: user.firstName, 
-        lastName: user.lastName, 
-        email: user.email, 
+      data: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
         roleId: user.role.id,
-        roleName: user.role.name 
-      }
+        roleName: user.role.name,
+      },
     });
 
-    let result = await dialogRef.afterClosed().toPromise();
-    if(result == "Canceled" || result === undefined) {
+    const result = await dialogRef.afterClosed().toPromise();
+    if (result === 'Canceled' || result === undefined) {
       this.customSnackBar.openSnackBar('Action annulée', 'notif-warning');
-    }else if(result !== undefined) {
-      this.customSnackBar.openSnackBar('L\'utilisateur a été modifié!', 'notif-success');
-      this.refreshList();
+    } else {
+      this.customSnackBar.openSnackBar(`L'utilisateur a été modifié!`, 'notif-success');
+      await this.refreshList();
     }
   }
 
   async openDeleteDialog(user: User) {
     const dialogRef = this.dialog.open(DeleteUserComponent, {
-      data: { 
-        id: user.id, 
-        firstName: user.firstName, 
-        lastName: user.lastName, 
-        email: user.email, 
+      data: {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
         roleId: user.role.id,
-        roleName: user.role.name
+        roleName: user.role.name,
       },
-      panelClass: 'warning-dialog'
+      panelClass: 'warning-dialog',
     });
 
-    let result = await dialogRef.afterClosed().toPromise();
-    if(result == "Canceled" || result == undefined){
+    const result = await dialogRef.afterClosed().toPromise();
+    if (result === 'Canceled' || result === undefined) {
       this.customSnackBar.openSnackBar('Action annulée', 'notif-warning');
-    } else if(result !== undefined) {
+    } else {
       try {
         await this.userService.deleteUser(result.id).toPromise();
-        this.customSnackBar.openSnackBar('L\'utilisateur a été supprimé!', 'notif-success');
-        this.refreshList();
-      } catch(error) {
-        this.customSnackBar.openSnackBar('Une erreur s\'est produit. Veuillez réessayer', 'notif-error');
+        this.customSnackBar.openSnackBar(`L'utilisateur a été supprimé!`, 'notif-success');
+        await this.refreshList();
+      } catch (error) {
+        this.customSnackBar.openSnackBar(`Une erreur s'est produite. Veuillez réessayer`, 'notif-error');
       }
     }
   }
@@ -141,14 +140,11 @@ export class UserListComponent implements OnInit {
     this.resetFilters();
   }
 
-  
-
   async refreshList() {
     this.userList = await this.userService.getAllUser().toPromise();
-    let userFilter = this.getUserFilterFromForm();
-    let filteredUsers = this.filterListFromUserFilter(userFilter, this.userList);
+    const userFilter = this.getUserFilterFromForm();
+    const filteredUsers = this.filterListFromUserFilter(userFilter, this.userList);
     this.dataSource = new MatTableDataSource<User>(filteredUsers);
     this.dataSource.paginator = this.paginator;
   }
-
 }
