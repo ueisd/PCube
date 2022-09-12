83d788cf9319705c8996c51df71ad2d6933bcea4
pierrelucueisd <pierrelucueisd@hotmail.com>
2022-09-12 17:54:26 -0400

    Added coverage to jest

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,jsx,ts}', '!**/node_modules/**', '!**/vendor/**'],
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
